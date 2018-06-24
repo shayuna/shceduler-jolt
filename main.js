15c42eb08589996cb712b1d8d9c7c547a9350c7b
shayuna <shaiz2206@gmail.com>
2018-06-24 13:18:45 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,22 +4,7 @@ require("./scheduler/scheduler.js");
 const app=express();
 
 app.get("*",(req,res,next)=>{
-/*
-    const database = firebase.database();
-    database
-    .ref("events").push({
-        dt:new Date(2018,5,20,12,50),
-        description:"just a simple fucking alert",
-        action:"alert('hello world')",
-    })
-    .then(()=>{
-        console.log ("push success");
-    })
-    .catch((err)=>{
-        console.log("err is - ",err);
-    });
-*/
-    res.send("ok1");
+    res.send("ok");
 });
 
 app.listen(8080);
